2ed46193f984bbc0aca5629304a530c32d7e0c2a
Natalie Villasana <navillasa@gmail.com>
2017-10-11 15:39:59 -0400

    begins turning Player into a class

--- server/classes.js
+++ server/classes.js
@@ -15,82 +15,82 @@ class Entity {
       self.y += self.speedY;
     }
   }
-// now i need to create an instance of entity
+// still need to create an instance of Entity
   
-  // player 
-  var Player = (id) => {
-    var self = Entity ();
-    self.id = id;
-    self.number = String(Math.floor(10 * Math.random()));
-    self.pressingRight = false;
-    self.pressingLeft = false;
-    self.pressingUp = false;
-    self.pressingDown = false;
-    self.maxSpeed = 5;
-  
-    var super_update = self.update;
-  
-    self.update = () => {
-        self.updateSpeed();
-        super_update();
+// player 
+class Player extends Entity {
+  constructor(id) {}
+  self.id = id;
+  self.number = String(Math.floor(10 * Math.random()));
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.maxSpeed = 5;
+
+  var super_update = self.update;
+
+  self.update = () => {
+      self.updateSpeed();
+      super_update();
+}
+  self.updateSpeed = () => {
+      if (self.pressingRight) {
+          self.speedX += self.maxSpeed;
+      }
+      else if (self.pressingLeft) {
+          self.speedX -= self.maxSpeed;
+      } else {
+          self.speedX = 0;
+      }
+
+      if (self.pressingUp) {
+          self.speedY -= self.maxSpeed;
+      }
+      else if (self.pressingDown) {
+          self.speedY += self.maxSpeed;
+      } else {
+          self.speedY = 0;
+      }
   }
-    self.updateSpeed = () => {
-        if (self.pressingRight) {
-            self.speedX += self.maxSpeed;
-        }
-        else if (self.pressingLeft) {
-            self.speedX -= self.maxSpeed;
-        } else {
-            self.speedX = 0;
-        }
-  
-        if (self.pressingUp) {
-            self.speedY -= self.maxSpeed;
-        }
-        else if (self.pressingDown) {
-            self.speedY += self.maxSpeed;
-        } else {
-            self.speedY = 0;
-        }
-    }
-    Player.obj[id] = self;
-    return self;
-  }
-  
-  Player.onConnect = (socket) => {
-    var player = Player(socket.id);
-    //receives keyPress directions from socket 
-    socket.on('keyPress', (data) => {
-        if (data.inputId === 'left') {
-          player.pressingLeft = data.state;
-        } else if (data.inputId === 'right') {
-          player.pressingRight = data.state;
-        } else if (data.inputId === 'up') {
-          player.pressingUp = data.state;
-        } else if (data.inputId === 'down') {
-          player.pressingDown = data.state;
-        }
+  Player.obj[id] = self;
+  return self;
+}
+
+Player.onConnect = (socket) => {
+  var player = Player(socket.id);
+  //receives keyPress directions from socket 
+  socket.on('keyPress', (data) => {
+      if (data.inputId === 'left') {
+        player.pressingLeft = data.state;
+      } else if (data.inputId === 'right') {
+        player.pressingRight = data.state;
+      } else if (data.inputId === 'up') {
+        player.pressingUp = data.state;
+      } else if (data.inputId === 'down') {
+        player.pressingDown = data.state;
+      }
+    });
+}
+
+Player.onDisconnect = (socket) => {
+  delete Player.obj[socket.id];
+}
+
+Player.update = () => {
+  var package = [];
+  for (var i in Player.obj) {
+    var player = Player.obj[i];
+      player.update();
+      package.push({
+        x:player.x,
+        y:player.y,
+        number:player.number
       });
   }
-  
-  Player.onDisconnect = (socket) => {
-    delete Player.obj[socket.id];
-  }
-  
-  Player.update = () => {
-    var package = [];
-    for (var i in Player.obj) {
-      var player = Player.obj[i];
-        player.update();
-        package.push({
-          x:player.x,
-          y:player.y,
-          number:player.number
-        });
-    }
-    return package;
-  }
-  
-  Player.obj = {};
-  
+  return package;
+}
+
+Player.obj = {};
+
 module.exports = Player;
